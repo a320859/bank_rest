661b02d6e98afbb19529300c012a64f23b070c60
a320859 <@.com>
2025-10-06 15:27:03 +0500

    feat: add @Operation descriptions to API endpoints

--- src/main/java/com/example/bankcards/controller/AdminController.java
+++ src/main/java/com/example/bankcards/controller/AdminController.java
@@ -2,6 +2,9 @@ package com.example.bankcards.controller;
 
 import com.example.bankcards.entity.Card;
 import com.example.bankcards.service.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Admin", description = "Administrative operations with cards and users")
 @RestController
+@SecurityRequirement(name = "Bearer Authentication")
 public class AdminController {
     private final AdminService adminService;
 
@@ -20,16 +25,28 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @Operation(
+            summary = "Delete card by id",
+            description = "Only the admin can delete a card."
+    )
     @DeleteMapping("/cards/{id}")
     public ResponseEntity<?> deleteCard(@PathVariable int id, @AuthenticationPrincipal UserDetails userDetails) {
         return adminService.deleteCard(id, userDetails);
     }
 
+    @Operation(
+            summary = "Delete user by id",
+            description = "Only the admin can delete the user."
+    )
     @DeleteMapping("/users/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable int id, @AuthenticationPrincipal UserDetails userDetails) {
         return adminService.deleteUser(id, userDetails);
     }
 
+    @Operation(
+            summary = "Get all cards that have block requests",
+            description = "Only the admin can see all cards with blocking requests"
+    )
     @GetMapping("/cards/blockRequests")
     public List<Card> getCardBlockRequests(@AuthenticationPrincipal UserDetails userDetails) {
         return adminService.getCardBlockRequests(userDetails);
